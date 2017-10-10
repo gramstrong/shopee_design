46bd5121e849d5d3d691f70f39505be768cca6df
Ken Huang <bluetch@gmail.com>
2017-10-11 00:12:14 +0800

    Revert "#1 replace jquery with react"

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -11,8 +11,6 @@ var menuName = ''
 var menu = ''
 var root = ''
 
-const apiUrl = 'https://api.rss2json.com/v1/api.json'
-
 for (var i = 0; i < 4; i++) {
 	if (i === 0) {
 		menuName = 'Seller Center Guideline';
@@ -49,114 +47,61 @@ function makeAddr(title) {
 	return addr;
 }
 
-function queryParams(params) {
-    return Object.keys(params)
-        .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-        .join('&');
-}
-
-function prepareMediumPost ({link, description, author, title, }){
-	const tagIndex = description.indexOf('<img');
-	const srcIndex = description.substring(tagIndex).indexOf('src=') + tagIndex;
-	const srcStart = srcIndex + 5;
-	const srcEnd = description.substring(srcStart).indexOf('"') + srcStart;
-	const img = description.substring(srcStart, srcEnd);
-
-	var yourString = description.replace(/<figure[^>]*>/g, "");
-	yourString = yourString.replace(/<img[^>]*>/g, "");
-	yourString = yourString.replace(/<hr>/g, "");
-	yourString = yourString.replace(/<strong[^>]*>/g, "");
-	yourString = yourString.replace(/<\/?a[^>]*>/g, "");
-	yourString = yourString.replace(/<strong[^>]*>/g, "");
-	yourString = yourString.replace(/<h[^>]*>/g, "");
-	yourString = yourString.replace(/<p[^>]*>/g, "");
-
-	var maxLength = 80;
-	if (yourString.indexOf('</p>') < maxLength) {
-		var trimmedString = yourString.substr(0, yourString.indexOf('</p>'));
-	} else {
-		trimmedString = yourString.substr(0, maxLength) + '...';
-	}
-	return {
-		link,
-		img,
-		title,
-		description: trimmedString,
-		author
-	}
-}
-
 export default class Home extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			mediumPosts: {
-				items: [],
-				isFetching: false,
-				error: null
-			}
-		}
-	}
-	updateMediumPostsState = newState => {
-		this.setState((prevState) => ({
-			mediumPosts: Object.assign(
-				{},
-				prevState.mediumPosts,
-				newState
-			)
-		}))
-	}
-	
-	fetchMediumPosts = () => {
-		var data = {
-			rss_url: 'https://medium.com/feed/shopee',
-			api_key: '2xozxhcwffkcirqd0s499i5odcbcfgo6ctzywvtm',
-			count: 3,
-			order_by:'pubDate'
-		};
-		this.updateMediumPostsState({isFetching: true});
-		fetch(`${apiUrl}?${queryParams(data)}`)
-			.then((response) => response.json())
-			.then((response) => {
-				console.log(response)
-				if(response.status !== 'ok'){
-					throw new Error("Server response is not ok");
-				}
-				const preparedItems = response.items.map(prepareMediumPost)
-				this.updateMediumPostsState({
-					isFetching: false,
-					items: preparedItems
-				});
-			})
-			.catch((error) => {
-				console.error(error);
-				this.updateMediumPostsState({
-					isFetching: false,
-					error
-				});
-			})
-	}
+
 	componentDidMount() {
-		this.fetchMediumPosts();
+		$(function () {
+			var $content = $('#content');
+			var data = {
+				rss_url: 'https://medium.com/feed/shopee',
+				// api_key: '2xozxhcwffkcirqd0s499i5odcbcfgo6ctzywvtm',
+				// count: 3,
+				// order_by:'pubDate'
+			};
+			$.get('https://api.rss2json.com/v1/api.json', data, function (response) {
+
+				if (response.status === 'ok') {
+					var output = '';
+					$.each(response.items, function (k, item) {
+						if (k > 2) {
+							return false;
+						}
+						var tagIndex = item.description.indexOf('<img');
+						var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex;
+						var srcStart = srcIndex + 5;
+						var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart;
+						var src = item.description.substring(srcStart, srcEnd);
+
+						output += '<a class="card col-md-4" href="' + item.link + '" target="_blank"><div class="crop"><img src="' + src + '" /></div>';
+						output += '<h3 class="title">' + item.title + '</h3>';
+
+						var yourString = item.description.replace(/<figure[^>]*>/g, "");
+						yourString = yourString.replace(/<img[^>]*>/g, "");
+						yourString = yourString.replace(/<hr>/g, "");
+						yourString = yourString.replace(/<strong[^>]*>/g, "");
+						yourString = yourString.replace(/<\/?a[^>]*>/g, "");
+						yourString = yourString.replace(/<strong[^>]*>/g, "");
+						yourString = yourString.replace(/<h[^>]*>/g, "");
+						yourString = yourString.replace(/<p[^>]*>/g, "");
+
+						var maxLength = 80
+						if (yourString.indexOf('</p>') < maxLength) {
+							var trimmedString = yourString.substr(0, yourString.indexOf('</p>'));
+						}
+						else {
+							trimmedString = yourString.substr(0, maxLength) + '...';
+						}
+
+						output += '<p class="desc">' + trimmedString + '</p>';
+						output += '<p class="author">By ' + item.author + '</p></a>';
+					});
+					$content.html(output);
+				}
+			});
+		});
 	}
-	renderMediumPostItem = ({link, img, title, description, author}, key) => {
-		return (
-			<div key={key} className="col-md-4">
-				<a className="card" href={link} target="_blank">
-					<div className="crop">
-						<img src={img} />
-					</div>
-					<h3 className="title">{title}</h3>
-					<p className="desc">
-						{description}
-					</p>
-					<p className="author">{author}</p>
-				</a>
-			</div>
-		)
-	}
+
 	render() {
-		const { mediumPosts } = this.state;
 		return (
 			<main className="home">
 				<div className="search">
@@ -209,23 +154,7 @@ export default class Home extends React.Component {
 				<div className="medium">
 					<div className="container">
 						<div className="title">Articles</div>
-						{
-							mediumPosts.isFetching ? 
-								(
-									<div className="col-xs-12">Loading Medium content...</div>
-								) : (
-									<div id="content">
-										{mediumPosts.items.map(this.renderMediumPostItem)}
-									</div>
-								)
-						}
-						{
-							mediumPosts.error ? (
-								<div className="col-xs-12">
-									<div className="alert alert-danger">{mediumPosts.error.message}</div>
-								</div>
-							) : null
-						}
+						<div id="content">Loading Medium content...</div>
 					</div>
 				</div>
 
